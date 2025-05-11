5e3a1b0e8802cfb17f75ffa5ad7dfb1a459564b1
NC-DEV-Léo <leo.verstraet@numericoach.fr>
2025-05-11 19:14:04 +0200

    feat/edited homeview and added mobile to git

--- apps/frontend/src/views/HomeView.vue
+++ apps/frontend/src/views/HomeView.vue
@@ -1,25 +1,10 @@
 <template>
     <div>
       <main class="p-4">
-        <div v-if="loggedIn">
+        <div>
           <h1 class="text-3xl font-bold mb-4">Bienvenue sur CESIZen</h1>
           <p>Vous pouvez accéder aux différentes fonctionnalités de l'application via le menu ci-dessus.</p>
         </div>
-        <div v-else>
-          <h1 class="text-3xl font-bold mb-4">Bienvenue sur CESIZen</h1>
-          <p>
-            Vous n'êtes pas connecté. Veuillez vous connecter ou vous inscrire pour accéder à l'ensemble des fonctionnalités de l'application.
-          </p>
-        </div>
       </main>
     </div>
-  </template>
-  
-  <script lang="ts" setup>
-  import { computed } from 'vue';
-  
-  // Vérifier la présence d'un token dans le localStorage pour déterminer si l'utilisateur est connecté.
-  const token = localStorage.getItem('token');
-  
-  const loggedIn = computed(() => !!token);
-  </script>
+</template>
