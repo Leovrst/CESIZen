9244901cba1d3d875d9dce943075b5e0d157cec4
NC-DEV-Léo <leo.verstraet@numericoach.fr>
2025-05-11 20:05:11 +0200

    feat: added ionic vue mobile app

---
+++ apps/mobile/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Accueil</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <div>
+        <h1 class="text-3xl font-bold mb-4">Bienvenue sur CESIZen</h1>
+        <p>Vous pouvez accéder aux différentes fonctionnalités de l'application via le menu ci-dessus.</p>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+</script>
