162eb5989b09ea9fb3adc12d4ee01327c1992b87
NC-DEV-Léo <leo.verstraet@numericoach.fr>
2025-05-18 20:40:27 +0200

    fix: questions and results creation and edition

--- apps/backend/src/diagnostic/dto/update-questions.dto.ts
+++ apps/backend/src/diagnostic/dto/update-questions.dto.ts
@@ -1,4 +1,11 @@
+import { IsInt, IsOptional, IsString } from "class-validator";
+
 export class UpdateQuestionDto {
+    @IsOptional()
+    @IsString()
     label?: string;
+
+    @IsOptional()
+    @IsString()
     points?: number;
 }
